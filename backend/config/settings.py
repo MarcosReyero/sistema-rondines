003efc702502d2f1ddbf23a051c3c5be6b916082
MarcosReyero <reyeromateo@gmail.com>
2026-05-19 14:07:56 -0300

    Agregar importación de os en settings.py para mejorar la gestión de variables de entorno

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,7 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-import os
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
