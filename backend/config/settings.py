59b8fa8b5c501bd30ea185171be9f7759cb9ff53
Marcos <reyeromateo@gmail.com>
2026-05-20 15:40:29 -0300

    fix: propagate X-Forwarded-Proto through proxy chain so DRF builds https:// media URLs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 LANGUAGE_CODE = 'es-ar'
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 USE_I18N = True
